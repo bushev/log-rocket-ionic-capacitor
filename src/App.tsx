8771170d5030061d09ac8865fa0b15ac6ca95774
Yury Bushev <bushevuv@gmail.com>
2021-05-11 14:06:10 -0400

    Add LR

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { Redirect, Route } from 'react-router-dom';
 import { IonApp, IonRouterOutlet } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
+import LogRocket from 'logrocket';
+
 import Home from './pages/Home';
 
 /* Core CSS required for Ionic components to work properly */
@@ -21,6 +23,8 @@ import '@ionic/react/css/display.css';
 
 /* Theme variables */
 import './theme/variables.css';
+
+LogRocket.init('ytei01/codesnack-ide');
 
 const App: React.FC = () => (
   <IonApp>
